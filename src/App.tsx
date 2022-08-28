c28e64d3054350d05bd3ff2f735b4663c4b90276
Alexandr Shishov <ernesto.chrome@mail.ru>
2022-08-28 23:07:36 +0300

    added material UI library, set up the first layout layout of the project;

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,43 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { AppBar, Box, Container, createTheme, Grid, IconButton, ThemeProvider, Toolbar, Typography } from '@mui/material';
+import MenuIcon from '@mui/material';
+import { cyan } from '@mui/material/colors';
+
 
 function App() {
+  const theme = createTheme({
+    palette: {
+      primary: cyan,
+      secondary: {
+        main: '#80deea',
+      },
+    },
+  })
   return (
     <div>
+      <ThemeProvider theme={theme}>
+        <Box sx={{ flexGrow: 2 }}>
+          <AppBar color='primary' position="relative">
+            <Toolbar>
+              <IconButton
+                size="medium"
+                edge="start"
+                color="default"
+                sx={{ mr: 5 }}
+              >
+              </IconButton>
+              <Typography variant="h4" component="div" sx={{ flexGrow: 1 }}>
+                Lorem
+              </Typography>
+            </Toolbar>
+          </AppBar>
+        </Box>
+        <Container>
 
+        </Container>
+      </ThemeProvider>
     </div>
   );
 }
